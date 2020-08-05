659faf2af1ee54045b1093d1cd220ae16810fa4b
ikizey <ikizey@gmail.com>
2020-08-05 16:22:28 +0300

    setup media folders in settings and urls

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('store.urls')),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
