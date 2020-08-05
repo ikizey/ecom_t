c6e25b5320749615e47a267ba1fdb6e3a03eaa90
ikizey <ikizey@gmail.com>
2020-08-05 16:24:26 +0300

    changed store view and template to show banners and items

--- store/views.py
+++ store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Banner, Item
 
 
 def store(request):
-    return render(request, 'store/store.html')
+    template = 'store/store.html'
+
+    banners = Banner.objects.all()
+    items = Item.objects.filter(is_active=True)
+
+    context = {'banners': banners, 'items': items}
+    return render(request, template, context)
 
 
 def cart(request):
