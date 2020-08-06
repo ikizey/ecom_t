bf85b4f1a3d8dd2b82dd8c97b91b8c559dfb5ee0
ikizey <ikizey@gmail.com>
2020-08-06 13:51:31 +0300

    added add_to_cart functionality

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import store, cart, item
+from .views import add_to_cart, store, cart, item
 
 urlpatterns = [
     path('', store, name="home"),
     path('cart', cart, name="cart"),
     path('items/<int:pk>/', item, name="item"),
+    path('items/<int:item_pk>/add_to_cart', add_to_cart, name="add_to_cart"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Banner, Item
+from django.shortcuts import render, redirect
+from .models import Banner, Item, Order, OrderItem
 
 
 def get_banners():
@@ -26,6 +26,19 @@ def item(request, pk):
     return render(request, template, context)
 
 
+def add_to_cart(request, item_pk):
+    user = request.user
+    if user.is_anonymous:
+        return redirect('login')
+
+    product = Item.objects.get(pk=item_pk, is_active=True)
+    order, _ = Order.objects.get_or_create(customer=user, complete=False)
+    order_item, _ = OrderItem.objects.get_or_create(order=order, item=product)
+
+    order_item.save()
+
+    return redirect('item', item_pk)
+
+
 def cart(request):
     return render(request, 'store/cart.html')
-
