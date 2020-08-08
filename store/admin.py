dfeb60a807040e76ebd6fea6c007676c2b87dc8c
ikizey <ikizey@gmail.com>
2020-08-09 01:15:34 +0300

    give default permissions to managers

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,44 @@
 from django.contrib import admin
+
 from .models import Banner, Brand, Item, Order
 
-admin.site.register(Banner)
-admin.site.register(Brand)
-admin.site.register(Item)
+
+class ManagerAdminMixin:
+    """Gives permission to manager users"""
+
+    def check_perm(self, user):
+        if user.is_superuser or user.manager:
+            return True
+        return False
+
+    def has_add_permission(self, request):
+        return self.check_perm(request.user)
+
+    def has_change_permission(self, request, obj=None):
+        return self.check_perm(request.user)
+
+    def has_delete_permission(self, request, obj=None):
+        return self.check_perm(request.user)
+
+    def has_module_permission(self, request):
+        return self.check_perm(request.user)
+
+
+@admin.register(Banner)
+class BannerAdmin(ManagerAdminMixin, admin.ModelAdmin):
+    search_fields = ['description']
+
+
+@admin.register(Brand)
+class BrandAdmin(ManagerAdminMixin, admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Item)
+class ItemAdmin(ManagerAdminMixin, admin.ModelAdmin):
+    list_display = ('fullname', 'is_active', 'rub_price', 'brand')
+    list_filter = ('is_active', 'brand')
+    search_fields = ['fullname']
+
+
 admin.site.register(Order)
