3e5414667b4631ecec6ef48ed573e2c4c1c1a79f
ikizey <ikizey@gmail.com>
2020-08-08 23:34:09 +0300

    fixed images path

--- store/models.py
+++ store/models.py
@@ -24,7 +24,7 @@ class Item(models.Model):
     )
 
     image = models.ImageField(
-        default='item_images/default.jpg', upload_to='items_images'
+        default='items_images/default.jpg', upload_to='items_images'
     )
 
     def __str__(self):
